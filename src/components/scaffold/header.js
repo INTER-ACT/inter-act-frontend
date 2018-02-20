defbf1d3e737857201883fde0bf536be45ed4228
0xEAB <desisma@heidel.beer>
2018-02-20 21:24:29 +0100

    Add logout button to header

--- src/components/scaffold/header.js
+++ src/components/scaffold/header.js
@@ -1,11 +1,24 @@
+import { inject } from 'aurelia-framework';
+import { AuthService } from '../../services/auth-service';
+
+@inject(AuthService)
 export class Header
 {
-    constructor()
+    constructor(authService: AuthService)
     {
+        this.authService = authService;
     }
 
     attached()
     {
         $(this).foundation();
+    }
+
+    submitLogout()
+    {
+        this.authService.logout().catch(error => {
+            alert('Logout failed. Check console.');
+            console.log(error);
+        });
     }
 }
