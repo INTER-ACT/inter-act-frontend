500bc9e6844c517cba0694f2187a2a6122278933
manuelweissensteiner <info@manuelweissensteiner.at>
2018-02-27 22:20:51 +0100

    Implement the startscreen

---
+++ src/components/startscreen.js
@@ -0,0 +1,12 @@
+import { inject } from 'aurelia-framework';
+import { DiscussionService } from '../services/discussion-service';
+
+
+export class Startscreen
+{
+    constructor()
+    {
+
+
+    }
+}
