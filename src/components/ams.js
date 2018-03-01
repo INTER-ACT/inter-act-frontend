f98a52928e7e032858c96983748ab96949a3420e
manuelweissensteiner <info@manuelweissensteiner.at>
2018-03-01 21:32:42 +0100

    Add CreateAmendent component

---
+++ src/components/ams.js
@@ -0,0 +1,9 @@
+import { inject } from 'aurelia-framework';
+import { DiscussionService } from '../services/discussion-service';
+
+export class Ams
+{
+    constructor(discussionService)
+    {
+    }
+}
