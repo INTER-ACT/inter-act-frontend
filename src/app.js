6acbc5fb17293274f76572f49bfcba2c5a4c42d0
manuelweissensteiner <info@manuelweissensteiner.at>
2018-03-09 00:39:01 +0100

    Implement privacy component

--- src/app.js
+++ src/app.js
@@ -127,6 +127,13 @@ export class App
                 moduleId: './components/download',
                 nav: true,
                 title: 'Download'
+            },
+            {
+                route: ['privacy'],
+                name: 'privacy',
+                moduleId: './components/privacy',
+                nav: true,
+                title: 'Privacy Download'
             }
         ]);
 
