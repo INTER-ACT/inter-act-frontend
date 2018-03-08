efb05ee5aa1a07487017b0316e075c84761b9cfa
0xEAB <desisma@heidel.beer>
2018-03-08 01:22:29 +0100

    Update discussion service

--- src/services/discussion-service.js
+++ src/services/discussion-service.js
@@ -19,8 +19,33 @@ export class DiscussionService
         return this.bsSrvc.getIntoJSON('discussions', {start: 0, count: 100});
     }
 
+    getAmendentById(id: number)
+    {
+        return this.bsSrvc.getIntoJSON('amendents/' + id);
+    }
+
+    getAmendents()
+    {
+        return this.bsSrvc.getIntoJSON('amendents', {start: 0, count: 100});
+    }
+
     getTags()
     {
         return this.bsSrvc.getIntoJSON('tags');
+    }
+
+    getTagById(id: number)
+    {
+        return this.bsSrvc.getIntoJSON('tags/' + id);
+    }
+
+    getCommentsByDiscussion(discussionID: number)
+    {
+        return this.bsSrvc.getIntoJSON('discussions/' + discussionID + '/comments');
+    }
+
+    getCommentById(id: number)
+    {
+        return this.bsSrvc.getIntoJSON('comments/' + id);
     }
 }
