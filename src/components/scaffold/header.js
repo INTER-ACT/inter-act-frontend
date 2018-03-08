6fff2a66c6881c380677b6f2bc3033a955bc81b2
0xEAB <desisma@heidel.beer>
2018-03-08 01:36:38 +0100

    Link to auth in header

--- src/components/scaffold/header.js
+++ src/components/scaffold/header.js
@@ -1,16 +1,18 @@
 import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
 import { AuthService } from '../../services/auth-service';
 
-@inject(AuthService)
+@inject(Router, AuthService)
 export class Header
 {
     isLoggedIn = false;
     isScientist = false;
     isAdmin = false;
 
-    constructor(authService: AuthService)
+    constructor(router: Router, authService: AuthService)
     {
         this.authService = authService;
+        this.router = router;
 
         this.isLoggedIn = this.authService.isLoggedIn();
 
@@ -21,6 +23,16 @@ export class Header
     attached()
     {
         $(this).foundation();
+    }
+
+    gotoLogin()
+    {
+        this.router.navigateToRoute('auth');
+    }
+
+    gotoSignup()
+    {
+        this.router.navigate('auth/signup');
     }
 
     submitLogout()
