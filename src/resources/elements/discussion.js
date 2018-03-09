b272f9737c9cbf55098c561017b7342d64753d21
manuelweissensteiner <info@manuelweissensteiner.at>
2018-03-09 01:35:01 +0100

    Update comment-function in discussion

--- src/resources/elements/discussion.js
+++ src/resources/elements/discussion.js
@@ -1,5 +1,9 @@
-import { bindable } from 'aurelia-framework';
+import { bindable, inject } from 'aurelia-framework';
+import { Author } from '../../models/author';
+import { AuthService } from '../../services/auth-service';
+import { DiscussionService } from '../../services/discussion-service';
 
+@inject(AuthService, DiscussionService)
 export class DiscussionCustomElement
 {
     @bindable rdata: object;
@@ -9,4 +13,55 @@ export class DiscussionCustomElement
      * DiscussionID
      */
     @bindable resourceid = '';
+
+    @bindable author: Author;
+    @bindable content: string;
+    @bindable rid: number;
+
+    hasCommentateBoxOpen: boolean = false;
+    replyText: string = '';
+
+
+
+
+    constructor(authService: AuthService, discussionService: DiscussionService)
+    {
+        this.authService = authService;
+        this.discussionService = discussionService;
+    }
+
+    commentateBegin()
+    {
+        if (!this.authService.isLoggedIn())
+        {
+            $('#plzLogin').foundation('reveal', 'open');
+            return;
+        }
+
+        this.hasCommentateBoxOpen = true;
+    }
+
+    report()
+    {
+        alert('not implemented yet');
+    }
+
+    submitComment()
+    {
+        this.discussionService.replyToComment(this.rid, this.replyText).then(r =>
+        {
+            alert(r);
+            this.discussionService.getCommentById(r.id).then(c => this.comments.push(c));
+        }).catch(error =>
+        {
+            alert('ERROR');
+            console.log(error);
+        });
+    }
+
+    submitCommentCancel()
+    {
+        this.replyText = '';
+        this.hasCommentateBoxOpen = false;
+    }
 }
