9ec5ff85f00ea6e0675059d3819c2130b68d335e
0xEAB <desisma@heidel.beer>
2018-03-12 16:43:16 +0100

    Use form locking on settings pages

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,8 +1,9 @@
 import { inject } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { UserService } from '../services/user-service';
+import { FormLocker } from '../utils/form-locker';
 
-@inject(UserService, Router)
+@inject(UserService, Router, FormLocker)
 export class Settings
 {
     pwChangeData: object = {
@@ -14,10 +15,12 @@ export class Settings
         email: ''
     };
 
-    constructor(userService: UserService, router: Router)
+    constructor(userService: UserService, router: Router, formLocker: FormLocker)
     {
         this.userService = userService;
         this.router = router;
+        this.formLocker = formLocker;
+
         this.insertEmail();
     }
 
@@ -35,25 +38,50 @@ export class Settings
 
     submitChangePassword()
     {
+        this.formLocker.lockForm(this.pwForm);
+
         if (this.pwChangeData.new_ !== this.pwChangeData.confirm_)
         {
             alert('Fehler: Passwörter ungleich!');
+            this.formLocker.unlockForm(this.pwForm);
             return;
         }
 
         this.userService.changePassword(this.pwChangeData.old, this.pwChangeData.new_).then(() =>
         {
             alert('Passwort erfolgreich geändert.');
+            this.formLocker.unlockForm(this.pwForm);
             this.pwForm.reset();
-        }).catch(() => alert('Fehlgeschlagen.'));
+        }, error =>
+        {
+            error.json().then(ej =>
+            {
+                if (ej.code == 3) // eslint-disable-line eqeqeq
+                {
+                    alert('Fehler:\nDas angegebene alte Passwort ist falsch.');
+                }
+                else
+                {
+                    alert('Unbekannter Fehler: ' + ej.message);
+                }
+
+                this.formLocker.unlockForm(this.pwForm);
+            });
+        });
     }
 
     submitChangeEmail()
     {
+        this.formLocker.lockForm(this.emailForm);
         this.userService.changeEmail(this.emailChangeData.email).then(() =>
         {
             alert('E-Mail-Adresse erfolgreich geändert.');
-        }).catch(() => alert('Fehlgeschlagen.'));
+            this.formLocker.unlockForm(this.emailForm);
+        }).catch(() =>
+        {
+            alert('Fehlgeschlagen.');
+            this.formLocker.unlockForm(this.emailForm);
+        });
     }
 
     gotoDownloadPrivacy()
