fbd78b2c8713e2321c62fa8ccec19bd9e4ad70ac
0xEAB <desisma@heidel.beer>
2018-04-03 14:14:58 +0200

    Fix sort value-converter

--- src/resources/value-converters/sort.js
+++ src/resources/value-converters/sort.js
@@ -9,8 +9,16 @@ export class SortValueConverter
         let factor = direction.match(/^desc*/i) ? 1 : -1;
         var retvalue = array.sort((a, b) =>
         {
-            var textA = a.toUpperCase ? a[property].toUpperCase() : a[property];
-            var textB = b.toUpperCase ? b[property].toUpperCase() : b[property];
+            if (!(a[property] && b[property]))
+            {
+                console.log('Sort error:');
+                console.log(a);
+                console.log(b);
+                return 0;
+            }
+
+            var textA = a[property].toUpperCase ? a[property].toUpperCase() : a[property];
+            var textB = b[property].toUpperCase ? b[property].toUpperCase() : b[property];
             return (textA < textB) ? factor : (textA > textB) ? -factor : 0;
         });
         return retvalue;
